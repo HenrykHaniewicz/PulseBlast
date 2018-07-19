0056b47cd6965625d9aba2e2353579fa77a5553f
Henryk Haniewicz (CHE - Student) <zhn11tau@ueadgkvwhlmj1g9.uea.ac.uk>
2018-07-19 10:20:49 +0100

    Changed the way files and directories are handled to incorporate argumenthandler module

--- PSRTiming.py
+++ PSRTiming.py
@@ -16,7 +16,7 @@ class Timing:
     TEMPO2 format, and creating fake TOAs based on user defined criteria.
     '''
 
-    def __init__( self, template, directory = None ):
+    def __init__( self, template, input, band):
 
         '''
         Initializes an instance of the class with a required template and an
@@ -25,12 +25,19 @@ class Timing:
         '''
 
         # Parse directory in string or choose CWD if no directory given
-        if directory == None:
-            self.directory = str( os.getcwd() )
-        else:
-            self.directory = str( directory )
+
+        self.directory = str( input )
 
         self.template = str( template )
+
+        if os.path.isdir( self.directory ):
+            self.getTOAs_dir( band )
+        elif os.path.isfile( self.directory ):
+            self.getTOAs_file( band )
+        else:
+            raise OSError( "File / directory does not exist." )
+
+
 
     def __repr__( self ):
         return "Timing( template = {}, directory = {} )".format( self.template, self.directory )
@@ -39,7 +46,7 @@ class Timing:
         return self.template, self.directory
 
 
-    def getTOAs( self, frequencyBand, save = None, exciseRFI = False ):
+    def getTOAs_dir( self, frequencyBand, save = None, exciseRFI = False ):
 
         '''
         Calculate and return Time-of-Arrivals (TOAs) for the given directory.
@@ -112,3 +119,81 @@ class Timing:
 
             else:
                 print( "{} is not a fits file...".format( self.file ) )
+
+
+    def getTOAs_file( self, frequencyBand, save = None, exciseRFI = False ):
+
+        '''
+        Calculate and return Time-of-Arrivals (TOAs) for the given directory.
+        Takes in a required argument for the frequency band (should match the template
+        band loaded in initialization). Either 'L' or '430'.
+        Each file can be chosen to undergo RFI excision before TOA calculation
+        '''
+
+
+        self.directory, self.file = os.path.split( self.directory )
+
+        # Check whether the file is a fits file
+        if self.file.endswith( ".fits" ) or self.file.endswith( ".refold" ):
+
+            # Check if the file is a calibration file (not included in the template)
+            if self.file.find( 'cal' ) == -1:
+
+                # Open the fits file header
+                hdul = fits.open( self.directory + "/" + self.file )
+
+                # Get the frequency band used in the observation.
+                frontend = hdul[0].header[ 'FRONTEND' ]
+
+                # Close the header once it's been used or the program becomes very slow.
+                hdul.close()
+
+                cullObject = DataCull( self.file, self.template, self.directory + "/", printFull = False )
+
+                if cullObject.SNError:
+                    pass
+
+                if exciseRFI:
+                    cullObject.reject( 'chauvenet', 15, True )
+
+                # Check which band the fits file belongs to
+                if frequencyBand == 'L':
+
+                    if frontend == 'lbw' or frontend == 'L_Band':
+
+                        cullObject.ar.tscrunch( nsubint = 6 )
+                        cullObject.ar.fscrunch( nchan = 1 )
+
+                        cullObject.ar.time( cullObject.template, filename = save, MJD = True )
+
+                    else:
+                        print( "430 band file" )
+
+                elif frequencyBand == '430':
+
+                    if frontend == '430':
+
+                        cullObject.ar.tscrunch( nsubint = 2 )
+                        cullObject.ar.fscrunch( nchan = 1 )
+
+                        cullObject.ar.time( cullObject.template, filename = save, MJD = True )
+
+                    else:
+                        print( "L-Band file" )
+
+
+                else:
+                    raise ArgumentError( "Frontend provided is neither 'L' nor '430'" )
+
+            else:
+                print( "Skipping calibration file..." )
+
+
+        else:
+            print( "{} is not a fits file...".format( self.file ) )
+
+
+class ArgumentError( Exception ):
+
+    def __init__( self, message ):
+        self.message = message
