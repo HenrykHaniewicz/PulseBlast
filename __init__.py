9c861f14a41653831b598c4c0d4905aead109cf9
Henryk Haniewicz (CHE - Student) <zhn11tau@ueadgkvwhlmj1g9.uea.ac.uk>
2018-08-02 10:19:52 +0100

    Created __init__ file for distribution purposes

---
+++ __init__.py
@@ -0,0 +1,9 @@
+__all__ = [ "main", "argumenthandler", "ArgumentHandler", "DataCulling", "DataCull", "PSRTemplate", "Template", "PSRTiming", "Timing", "mathUtils", "otherUtilities", "custom_exceptions", "ArgumentError" ]
+
+__version__ = 0.1
+
+from argumenthandler import ArgumentHandler
+from DataCulling import DataCull
+from PSRTemplate import Template
+from PSRTiming import Timing
+from custom_exceptions import *
