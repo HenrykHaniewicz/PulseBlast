215d8dfba677626eb679bfe348018db679dd4c4d
Henryk T. Haniewicz <henrykt.haniewicz@henryks-mbp.home>
2018-09-11 23:58:28 +0100

    Added utilities for directory strings

--- otherUtilities.py
+++ otherUtilities.py
@@ -1,9 +1,10 @@
-# Extra utilities for pulsars
+# Extra utilities for pulsars and other stuff
 
 # Imports
 import numpy as np
 import math
 import sys
+import platform
 import mathUtils as mu
 
 # Functions
@@ -110,3 +111,53 @@ def display_status( iteration, MAX_ITER ):
 
     sys.stdout.write('{0:<10d}[{1:>3d}%]'.format( iteration, int( 100 * float( iteration )/float( MAX_ITER ) ) ) )
     sys.stdout.flush()
+
+
+# Formats directories from UI to console
+def formatMultipleDirectories( args ):
+    if platform.system() == 'Darwin' or 'Linux':
+        dirs = ' '.join( args )
+        dirs = dirs.replace( ":", "," )
+        dirs = dirs.replace( "Macintosh HD", "" )
+        dirs = dirs.split( "," )
+
+    elif platform.system() == 'Windows':
+        dirs = args
+
+    else:
+        raise OSError( "Could not determine OS." )
+
+    return dirs
+
+
+def addMultipleDirectoryEndSeparators( dirs, shell ):
+    if shell == 'Unix':
+        for i, d in enumerate( dirs ):
+            if not d.endswith("/"):
+                dirs[i] += "/"
+
+    elif shell == 'Windows':
+        for i, d in enumerate( dirs ):
+            if not d.endswith("\\"):
+                dirs[i] += "\\"
+
+    else:
+        raise ValueError( "Shell not recognized. (Shell provided: {})".format( shell ) )
+
+    return dirs
+
+
+# Takes in a single directory followed by the rest in a list. I'm gonna change this...
+def addDirectoryEndSeparators( dir, dirs ):
+    if platform.system() == 'Darwin' or "Linux":
+        directory = dir + "/"
+        directories = addMultipleDirectoryEndSeparators( dirs, 'Unix' )
+
+    elif platform.system() == "Windows":
+        directory = dir + "\\"
+        directories = addMultipleDirectoryEndSeparators( dirs, 'Windows' )
+
+    else:
+        raise OSError( "Could not determine OS." )
+
+    return directory, directories
