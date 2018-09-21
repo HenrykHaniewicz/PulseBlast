d111c1be555dd0e70146759dd3a09b59904877f4
Henryk Haniewicz (CHE - Student) <zhn11tau@ueadgkvwhlmj1g9.uea.ac.uk>
2018-09-21 09:56:53 +0100

    Put utilities in separate directory

---
+++ utils/mathUtils.py
@@ -0,0 +1,61 @@
+# Custom math utilities in python 3
+# Henryk T. Haniewicz, 2018
+
+# Imports
+import numpy as np
+
+# Functions
+
+def rootMeanSquare( array ):
+
+    '''
+    Returns the RMS of a data array
+    '''
+
+    rms = np.sqrt( np.mean( np.power( array, 2 ) ) )
+    return rms
+
+
+def chauvenet( array, mean = 0, stddev = 1, threshold = 3.0 ):
+
+    '''
+    Returns a boolean array of the same shape as the input array based on the
+    Chauvenet outlier criterion.
+    Default values for the mean and stddev are for a normalized Gaussian but
+    it is more useful to use values calculated from your array.
+    '''
+
+    absDiff = abs( array - mean )
+
+    return absDiff > ( threshold * stddev )
+
+
+def doubleMAD( vector, threshold = 3.5 ):
+
+    '''
+    Returns a boolean array comparing the Modified Z-Score (MZS) to the given threshold factor.
+    Only works with 1D arrays (vectors) but can be iterated over for multiple distributions.
+    A return of True implies an outlying data point.
+    '''
+
+    # Calculate the overall median (allows for masked vectors)
+    m = np.ma.median( vector )
+
+    # Calculate the absolute deviation from the true median
+    absDev = np.abs( vector - m )
+
+    # Calculate the median absolute deviation for both the left and right splits
+    leftMAD = np.ma.median( absDev[vector <= m] )
+    rightMAD = np.ma.median( absDev[vector >= m] )
+
+    vectorMAD = leftMAD * np.ones( len( vector ) )
+    vectorMAD[vector > m] = rightMAD
+
+    # Calculate the modified Z score
+    MZS = 0.6745 * absDev / vectorMAD
+
+    # If the value of the vector equals the median, set the MZS to 0
+    MZS[vector == m] = 0
+
+    # Return true if the MZS is greater than the threshold
+    return MZS > threshold
---
+++ utils/otherUtilities.py
@@ -0,0 +1,109 @@
+# Extra utilities
+
+# Imports
+import sys
+import os
+import platform
+import numpy as np
+
+
+def zeroWeights( criterion = None, archive = None, verbose = False ):
+
+    '''
+    Scans through each sub-integration and channel and sets any bad scans to zero.
+    '''
+
+    if criterion is None:
+        raise ValueError( "Please provide a rejection criterion" )
+    elif archive is None:
+        raise ValueError( "Please provide an archive" )
+
+    for time, frequency in zip( np.where( criterion )[0], np.where( criterion )[1] ):
+        if verbose:
+            print( "Setting the weight of (subint: {}, channel: {}) to 0".format( time, frequency ) )
+        archive.setWeights( 0, t = time, f = frequency )
+
+# Delete and flush the current line on the console
+def restart_line():
+     sys.stdout.write( '\r' )
+     sys.stdout.flush()
+
+# Simple progress bar
+def display_status( iteration, MAX_ITER ):
+    restart_line()
+
+    sys.stdout.write('{0:<10d}[{1:>3d}%]'.format( iteration, int( 100 * float( iteration )/float( MAX_ITER ) ) ) )
+    sys.stdout.flush()
+
+
+def addExtension( file, ext ):
+
+    '''
+    Add any desired extension to a file that doesn't have one.
+    If the file does, that extension will be used instead.
+    '''
+
+    if not isinstance( ext, str ):
+        raise TypeError( "Extension must be a string" )
+
+    # Split the filename up into a root and extension
+    root, end = os.path.splitext( file )
+
+    # If file extension does not exist, add the extension
+    if not end:
+        end = '.' + ext
+        fileout = root + end
+    else:
+        fileout = file
+
+    return fileout
+
+
+# Formats directories from GUI output to console. Need to test on Windows...
+def formatMultipleDirectories( args ):
+    if platform.system() == 'Darwin' or 'Linux':
+        dirs = ' '.join( args )
+        dirs = dirs.replace( ":", "," )
+        dirs = dirs.replace( "Macintosh HD", "" )
+        dirs = dirs.split( "," )
+
+    elif platform.system() == 'Windows':
+        dirs = args
+
+    else:
+        raise OSError( "Could not determine OS." )
+
+    return dirs
+
+# Adds the correct separator (based on platform) to the end of the directories parsed.
+def addMultipleDirectoryEndSeparators( dirs, shell ):
+    if shell == 'Unix':
+        for i, d in enumerate( dirs ):
+            if not d.endswith("/"):
+                dirs[i] += "/"
+
+    elif shell == 'Windows':
+        for i, d in enumerate( dirs ):
+            if not d.endswith("\\"):
+                dirs[i] += "\\"
+
+    else:
+        raise ValueError( "Shell not recognized. (Shell provided: {})".format( shell ) )
+
+    return dirs
+
+
+# Takes in a single directory followed by the rest in a list. I'm gonna change this...
+def addDirectoryEndSeparators( dir, dirs ):
+    if platform.system() == 'Darwin' or "Linux":
+        directory = dir + "/"
+        directories = addMultipleDirectoryEndSeparators( dirs, 'Unix' )
+
+    elif platform.system() == "Windows":
+        directory = dir + "\\"
+        directories = addMultipleDirectoryEndSeparators( dirs, 'Windows' )
+
+    else:
+        raise OSError( "Could not determine OS." )
+
+    return directory, directories
