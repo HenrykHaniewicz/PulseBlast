7581c96ae615a91aedb4dfa1fffd50d44f7105ac
Henryk Haniewicz (CHE - Student) <zhn11tau@ueadgkvwhlmj1g9.uea.ac.uk>
2018-09-26 16:03:34 +0100

    Added contour plotting functions

--- utils/pulsarUtilities.py
+++ utils/pulsarUtilities.py
@@ -99,3 +99,108 @@ def removeBase( profData, duty ):
      profData = profData - baseline
 
      return profData
+
+
+
+
+def loadContourArrays( fileprefix ):
+
+
+    load_array = np.load(fileprefix+'_params.npy')
+    # for the purposes of this routine, only need the following
+    # things in p_out
+    p_out = {'m2':load_array[0],
+             'mtot':load_array[1],
+             'm1':load_array[2],
+             'm1_prob':load_array[3]}
+    p_out['norm_like'] = np.load(fileprefix+'_prob.npy')
+
+    return p_out
+
+
+def plot_contour_pdf(x_val, y_val, contour_data, n_steps=32,\
+                         norm=False, weights=None, \
+                         canvassize=None, xticks=True, yticks=True, \
+                         xlabel=True, ylabel=True, linecolour='black', \
+                         xlim=None, ylim=None, figtext=None, figtextsize=16, \
+                         hgrid=False, vgrid=False,
+                         ticklabelsize=18, axislabelsize=18):
+
+# If weights are None, assign them to ones, with the same shape as the
+# input z array:
+    if (weights==None):
+        weights = np.ones_like(contour_data, dtype=float)
+# If weights are given, ensure they are the same shape as z:
+    else:
+        if(weights.shape != contour_data.shape):
+            print 'Shape of weight array ', weights.shape, \
+                ' does not match the input data array ', contour_data.shape
+            return None
+
+# Start by setting up lot limits.  Assuming 1-D array input:
+    xmin = np.min(x_val)
+    xmax = np.max(x_val)
+    ymin = np.min(y_val)
+    ymax = np.max(y_val)
+    xspan = abs(xmax - xmin)
+    yspan = abs(ymax - ymin)
+
+# Set up the plot:
+    fig = plt.figure(figsize=canvassize)
+    ax = fig.add_axes([0.28, 0.19, 0.68, 0.76])
+    ax.xaxis.set_tick_params(labelsize=ticklabelsize, pad=8)
+    ax.yaxis.set_tick_params(labelsize=ticklabelsize, pad=8)
+    ax.ticklabel_format(axis='x', useOffset=False)
+    ax.ticklabel_format(axis='y', useOffset=False)
+    if(xlim==None):
+        ax.set_xlim(xmin - 0.01*xspan, xmax + 0.02*xspan)
+    else:
+        ax.set_xlim(xlim)
+    if(ylim==None):
+        ax.set_ylim(ymin - 0.01*yspan, ymax + 0.02*yspan)
+    else:
+        ax.set_ylim(ylim)
+
+    if (xlabel!=None):
+        ax.set_xlabel(xlabel, fontsize=axislabelsize, labelpad=12)
+    if (ylabel!=None):
+        ax.set_ylabel(ylabel, fontsize=axislabelsize, labelpad=12)
+
+    if(not xticks):
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label1On = False
+            tick.label2On = False
+
+    if(not yticks):
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label1On = False
+            tick.label2On = False
+
+    if(hgrid):
+        ax.yaxis.grid(linestyle='--', color='black', \
+                          linewidth=0.4)
+    if(vgrid):
+        ax.xaxis.grid(linestyle='--', color='black', \
+                          linewidth=0.4)
+
+    prob_intervals = np.array([0.683, 0.954, 0.9973])
+
+# Create levels at which to plot contours at each of the above intervals.
+# Will not assume going in that Z values are normalized to total volume of 1.
+    contour_level = get_prob_2D_levels(contour_data, prob_intervals, n_steps=n_steps)
+    print "CONTOUR_LEVEL = ", contour_level
+
+    if (norm==True):
+        z_val = (contour_data*weights)/np.sum(contour_data*weights)
+    else:
+        z_val = contour_data
+
+# Now plot the pdf data
+    ax.contour(x_val, y_val, z_val, levels=np.flip(contour_level, axis=0), \
+                   colors=('red', 'blue', 'green'))
+
+    if(figtext!=None):
+        for txt in figtext:
+            ax.text(txt[0], txt[1], txt[2], fontsize=figtextsize, \
+                        horizontalalignment='center', \
+                        verticalalignment='center',)
