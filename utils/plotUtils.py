9175dd8d7d19f7d5fb4e4f47a06fa8d4c044f575
Henryk T. Haniewicz <henrykt.haniewicz@henryks-mbp.home>
2018-11-25 13:43:57 +0000

    Histogram function

--- utils/plotUtils.py
+++ utils/plotUtils.py
@@ -1,58 +1,116 @@
 # Plotting utilities
 
 # Imports
+from custom_exceptions import DimensionError
 import random
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 color_list = [ 'b', 'g', 'r', 'c', 'y', 'm' ]
 
 
-def histogram_and_curves( array, mean = 0.0, stdDev = 1.0, bins = None, xAxis = 'X', yAxis = 'Y', zAxis = 'Z', show = True, *curves ):
+def histogram_and_curves( array, mean = 0.0, std_dev = 1.0, bins = None, x_lims = None, y_lims = None, x_axis = 'X', y_axis = 'Y', title = 'Title', show = True, filename = None, curve_list = None, **kwargs ):
 
     """
-    Plots a histogram of a data array in 1 or 2 dimensions.
+    Plots a histogram of a data array in 1 or 2 dimensions and an arbitrary number of PDFs for comparison.
     """
 
     color = 'k'
     bgcolor = 'w'
-    style = 'bar'
+    style = 'step'
 
     fig = plt.figure( figsize = (6,6) )
     ax = fig.add_subplot( 111, facecolor = bgcolor )
-
-    if bins is None:
-        bins = np.arange( np.amin( array ), np.amax( array ) )
-
-    XMIN = mean - ( 4 * stdDev )
-    XMAX = mean + ( 4 * stdDev )
-
-    ax.set_xlim( XMIN, XMAX )
-    ax.set_ylim( 0, 1 )
-    t = np.arange( XMIN , XMAX, 0.01)
+    xText = ax.set_xlabel( x_axis )
+    yText = ax.set_ylabel( y_axis )
+    title = ax.set_title( title )
 
     if array.ndim is 1:
 
-        # Plot the 1D histogram
-        n, bins, patches = ax.hist( array, bins = bins, density = True, color = color, histtype = style )
+        if bins is None:
+            step = ( math.ceil( np.amax( array ) ) - math.floor( np.amin( array ) ) ) / 50
+            bins = np.arange( math.floor( np.amin( array ) ), math.ceil( np.amax( array ) ), step )
 
-        ax.plot( t, spyst.norm.pdf(t), color = random.choice( color_list ) )
-        ax.plot( t, spyst.vonmises.pdf(t, 1), color = random.choice( color_list ) )
+        if not x_lims:
+            x_min = mean - ( 4 * std_dev )
+            x_max = mean + ( 4 * std_dev )
+        else:
+            x_min, x_max = x_lims
+
+        t = np.arange( x_min , x_max, 0.01)
+
+        # Plot the 1D histogram
+        n, bins, patches = ax.hist( array, bins = bins, density = True, color = color, histtype = style, linewidth = 2 )
+
+        xlim = ax.set_xlim( x_min, x_max )
+        ylim = ax.set_ylim( 0, 1.2 * np.amax( n ) )
 
         # Plot distribution curves
-        #if curves:
-        #    for curve in curves:
-        #        ax.plot( t, curve, color = random.choice( dashed_color_list ) )
-                #ax.plot( t, curve, color = 'r' )
+        if curve_list:
+            for curve in curve_list:
+                try:
+                    params = opt.curve_fit( curve, bins[1:], n, p0 = [0.5, 0.5] )
+                    ax.plot( t, curve( t, *params[0] ), color = random.choice( color_list ), linewidth = 2 )
+                except TypeError:
+                    ax.plot( t, curve( t ), color = random.choice( color_list ), linewidth = 2 )
 
+        plt.grid( True )
+
+        if filename is not None:
+            plt.savefig( filename )
         if show:
             plt.show()
+        else:
+            plt.close()
+
+    elif array.ndim is 2 and array.shape[0] is 2:
+
+
+        # Basically determines whether mean given was [float, float] or float
+        if not x_lims:
+            try:
+                x_min = mean[0] - ( 4 * std_dev[0] )
+                x_max = mean[0] + ( 4 * std_dev[0] )
+            except TypeError:
+                x_min = mean - ( 4 * std_dev )
+                x_max = mean + ( 4 * std_dev )
+        else:
+            x_min, x_max = x_lims
+
+        if not y_lims:
+            try:
+                y_min = mean[1] - ( 4 * std_dev[1] )
+                y_max = mean[1] + ( 4 * std_dev[1] )
+            except TypeError:
+                y_min = x_min
+                y_max = x_max
+        else:
+            y_min, y_max = y_lims
+
+        # Initialize bin size if not parsed in
+        if bins is None:
+            step = ( math.ceil( np.amax( array ) ) - math.floor( np.amin( array ) ) ) / 500
+            bins = [ np.arange( math.floor( np.amin( array[0] ) ), math.ceil( np.amax( array[0] ) ), step ), np.arange( math.floor( np.amin( array[1] ) ), math.ceil( np.amax( array[1] ) ), step ) ]
+
+        # Plot the 2D histogram
+        h, x_edge, y_edge, quad_mesh = ax.hist2d( array[0], array[1], bins = bins )
+
+        xlim = ax.set_xlim( x_min, x_max )
+        ylim = ax.set_ylim( y_min, y_max )
+
+        if filename is not None:
+            plt.savefig( filename )
+        if show:
+            plt.show()
+        else:
+            plt.close()
 
     elif array.ndim is 2:
-        raise ValueError()
-
+        raise DimensionError( "Invalid array shape. Number of rows required: 2. (Actual: {})".format( array.shape[0] ) )
     else:
-        print( "Invalid dimensions. Required: 1 or 2. (Actual: {})".format( array.ndim ) )
+        raise DimensionError( "Invalid dimensions. Required: 2. (Actual: {})".format( array.ndim ) )
 
     return ax
 
@@ -76,29 +134,31 @@ def waterfall( array, ax = None, offset = None, border = 0, labels = True, bins 
             bins = np.arange( array.shape[1] )
 
 
-        XMIN = 0
-        XMAX = len( bins ) - 1
-        YMIN = 0 - offset
-        YMAX = ( 1 + len( array ) ) * offset
-        XLOW = XMIN - ( XMAX - XMIN ) * border
-        XHIGH = ( XMAX - XMIN ) * border + XMAX
-        YLOW = YMIN - ( YMAX - YMIN ) * border
-        YHIGH = ( YMAX - YMIN ) * border + YMAX
+        x_min = 0
+        x_max = len( bins ) - 1
+        y_min = 0 - offset
+        y_max = ( 1 + len( array ) ) * offset
+        x_low = x_min - ( x_max - x_min ) * border
+        x_high = ( x_max - x_min ) * border + x_max
+        y_low = y_min - ( y_max - y_min ) * border
+        y_high = ( y_max - y_min ) * border + y_max
 
 
         for i in np.arange( len( array ) ):
             ax.plot( array[i][bins] + offset * i, color )
 
-        ax.set_xlim( XLOW, XHIGH )
-        ax.set_ylim( YLOW, YHIGH )
+        ax.set_xlim( x_low, x_high )
+        ax.set_ylim( y_low, y_high )
 
         if not labels:
             ax.set_xticklabels([])
             ax.set_yticklabels([])
         if show:
             plt.show()
+        else:
+            plt.close()
     else:
-        print( "Invalid dimensions. Required: 2. (Actual: {})".format( array.ndim ) )
+        raise DimensionError( "Invalid dimensions. Required: 2. (Actual: {})".format( array.ndim ) )
 
     return ax
 
@@ -121,7 +181,8 @@ def greyscale( array, ax = None, cbar = False, mask = None, show = True, filenam
         else:
             plt.close()
     else:
-        print( "Invalid dimensions. Required: 2. (Actual: {})".format( array.ndim ) )
+        raise DimensionError( "Invalid dimensions. Required: 2. (Actual: {})".format( array.ndim ) )
+
     return ax
 
 def imshow( x, ax = None, origin = 'lower', interpolation = 'nearest', aspect = 'auto', **kwargs ):
@@ -132,14 +193,17 @@ def imshow( x, ax = None, origin = 'lower', interpolation = 'nearest', aspect = 
     return im
 
 
-
+# FOR TESTING
 if __name__ == "__main__":
 
     import scipy.stats as spyst
     import scipy.optimize as opt
 
-    array = np.random.vonmises( mu = 0, kappa = 1, size = 64 )
+    array = np.array([ np.random.normal( loc = 0, scale = 1, size = 2000000 ), np.random.vonmises( kappa = 1, mu = 0, size = 2000000 ) ])
 
-    print(array)
+    x_axis = "PDF in A"
+    y_axis = "PDF in B"
 
-    histogram_and_curves( array )
+    title = r'$\mu_{{0}}={},\ \sigma_{{0}}={},\ \mu_{{1}}={},\ \sigma_{{1}}={}$'.format( 0, 1, 0, 1 )
+
+    histogram_and_curves( array, [0, 0], [1, 1], None, None, [-np.pi, np.pi], x_axis, y_axis, title, True, None, [ spyst.norm.pdf, spyst.vonmises.pdf ] )
